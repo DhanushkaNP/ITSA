b17aa0141b4ddb5fa30ce3de8343ef6cf00bef04
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-06-01 22:20:58 +0530

    Update index.js - Added res header files, and added logic to send users errors if there any. And also change the logic to connect database using connectToDatabase()

--- server/index.js
+++ server/index.js
@@ -1,7 +1,38 @@
 const express = require("express");
+const connectToDatabase = require("./db/mongoose");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
+app.get("/", (req, res, next) => res.send("Hello World!"));
+
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    next(err);
+  } else {
+    res.status(err.code || 500);
+    res.json({ message: err.message || "An unknown error occurred!" });
+  }
+});
+
+connectToDatabase()
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
