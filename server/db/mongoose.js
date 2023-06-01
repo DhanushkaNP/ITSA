460f866a1ca63a2ef9cde7de26bfcbd1ab7b209b
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-06-01 22:14:54 +0530

    Update mongoose.js -Added logic to connect mongodb using mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,1 +1,14 @@
 //DB_CONNECTION_STRING = This is the key to access db connection string.
+const mongoose = require("mongoose");
+const HttpError = require("../models/Http-Error");
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw new Error("Something going wrong with database connection");
+  }
+};
+
+module.exports = connectToDatabase;
