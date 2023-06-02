87c945576f53244f2ace4849ddc87642db4cb650
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-06-02 12:57:19 +0530

    Update blogs-routes.js : Import places controlles.

--- server/routes/blogs-routes.js
+++ server/routes/blogs-routes.js
@@ -1,22 +1,13 @@
 const express = require("express");
+const blogController = require("../controllers/blogs-controllers");
 const router = express.Router();
 
-router.get("/all", function (req, res, next) {
-  res.send("All blogs");
-});
+router.get("/all", blogController.getAllBlogs);
 
-router.post("/new", function (req, res, next) {
-  res.send("Added new blog post");
-});
+router.post("/new", blogController.createNewBlog);
 
-router.patch("/:bid", function (req, res, next) {
-  const blogId = req.params.bid;
-  res.send(`blog ${blogId} updated`);
-});
+router.patch("/:bid", blogController.updateBlog);
 
-router.delete("/:bid", function (req, res, next) {
-  const blogId = req.params.bid;
-  res.send(`member ${blogId} deleted`);
-});
+router.delete("/:bid", blogController.deleteBlog);
 
 module.exports = router;
