09ed4afadbfb05c160d277b388991301ee009277
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-06-02 10:42:00 +0530

    Moved: http-error.js to new util folder

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,5 @@
 //DB_CONNECTION_STRING = This is the key to access db connection string.
 const mongoose = require("mongoose");
-const HttpError = require("../models/Http-Error");
 
 const connectToDatabase = async () => {
   try {
