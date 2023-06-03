7de22f43ffda65aa04415c2150b3fcec500c7ae3
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-06-03 15:23:54 +0530

    Update admin middleware to accept id from req.

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -1,4 +1,9 @@
+const jwt = require("jsonwebtoken");
+const { tokenHeader } = require("../config.json");
+
 module.exports = (req, res, next) => {
-  if (req.user.type !== "Admin") return res.status(403).send("Access denied.");
+  const token = req.header(tokenHeader);
+  const user = jwt.decode(token);
+  if (user.type !== "Admin") return res.status(403).send("Access denied.");
   next();
 };
