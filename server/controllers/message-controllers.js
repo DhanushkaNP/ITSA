5c5bf737dbfca96cd6731b6033598c1c836401a0
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-06-10 23:04:45 +0530

    In createNewMessage controller Add logic to create new message

--- server/controllers/message-controllers.js
+++ server/controllers/message-controllers.js
@@ -1,9 +1,27 @@
+const Message = require("../models/message");
+const HttpError = require("../util/http-Error");
+
 async function getAllMessages(req, res, next) {
   res.send("All Messages");
 }
 
 async function createNewMessage(req, res, next) {
-  res.send("Added new Message");
+  const { name, email, phone, message } = req.body;
+
+  const newMessage = Message({
+    name,
+    email,
+    phone,
+    message,
+  });
+
+  try {
+    await newMessage.save();
+  } catch (err) {
+    return next(new HttpError("Couldn't create a message", 500));
+  }
+
+  res.status(201).send("Message created");
 }
 
 exports.getAllMessages = getAllMessages;
