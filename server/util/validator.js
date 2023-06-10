8476c9262d1a80586d6837a2546ac9a33a603909
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-06-10 21:57:07 +0530

    In validator.js Create a new memberAuthSchema for Joi validation

--- server/util/validator.js
+++ server/util/validator.js
@@ -6,6 +6,13 @@ const eventAuthSchema = Joi.object({
   date: Joi.date().iso().required(),
 });
 
+const memberAuthSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  position: Joi.string().min(2).required(),
+  description: Joi.string().required(),
+});
+
 module.exports = {
   eventAuthSchema,
+  memberAuthSchema,
 };
