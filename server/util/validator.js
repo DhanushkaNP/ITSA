dd494d9ce9e336030534cfabf6cd25139e06cb50
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-06-11 12:27:02 +0530

    Added new Joi schema for message validation

--- server/util/validator.js
+++ server/util/validator.js
@@ -12,7 +12,15 @@ const memberAuthSchema = Joi.object({
   description: Joi.string().required(),
 });
 
+const messageAuthSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  email: Joi.string().email().required(),
+  message: Joi.string().min(2).required(),
+  phone: Joi.string().pattern(/^\+[1-9]\d{4,12}$/),
+});
+
 module.exports = {
   eventAuthSchema,
   memberAuthSchema,
+  messageAuthSchema,
 };
